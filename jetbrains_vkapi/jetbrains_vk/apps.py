a54f53735b3d04055dcd8f5d997c3c65669d827e
Podlevskiy Viktor <vitca64rus@gmail.com>
2021-04-08 10:13:13 +0300

    make django_app and page index.html

---
+++ jetbrains_vkapi/jetbrains_vk/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JetbrainsVkConfig(AppConfig):
+    name = 'jetbrains_vk'
--- jetbrains_vkapi/jetbrains_vkapi/urls.py
+++ jetbrains_vkapi/jetbrains_vkapi/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from jetbrains_vk import views
+from django.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('', views.index, name='index'),
+    url(r'^admin/', admin.site.urls),
 ]
