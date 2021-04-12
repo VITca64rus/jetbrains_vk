ffc62cae8cadb41b143acf773228862a4187f2a3
Podlevskiy Viktor <vitca64rus@gmail.com>
2021-04-12 23:43:46 +0300

    fix bag load to DB and make all graph

--- jetbrains_vkapi/jetbrains_vk/views.py
+++ jetbrains_vkapi/jetbrains_vk/views.py
@@ -16,6 +16,7 @@ def index(request):
         api = vk.API (session)
 
 
+        """
         # Получаю массив id_posts с ид всех постов сообщества/человека
         wall=api.wall.get(owner_id=int('-{}'.format(domain)),count=1,v=5.71)
         count_wall=int(wall['count'])
@@ -41,6 +42,7 @@ def index(request):
                 offset_comments += 100
                 count_comments = comments['count']
                 for comment in comments['items']:
+                    print(comment)
                     try:
                         all_comments.append([comment['from_id'], comment['likes']['count'], comment['date']])
                     except:
@@ -63,13 +65,15 @@ def index(request):
 
             #Загружаю все комментарии поста в БД
             print('НАЧАЛ ЗАГРУЗКУ В БД')
-            comments_bd = Choice()
+            print(all_comments)
+            #comments_bd = Choice()
             for comment_ in all_comments:
+                comments_bd = Choice ()
                 comments_bd.id_user = comment_[0]
                 comments_bd.date = time.strftime("%D", time.localtime(int(comment_[2])))
                 comments_bd.likes_count = comment_[1]
                 comments_bd.save()
-
+"""
       #  print(time.strftime("%D", time.localtime(int('1284101485'))))
 
         #Делаю запросы к БД строю графики
@@ -90,16 +94,47 @@ def index(request):
             user1.append (user_count ['id_user'])
             count1.append ((user_count ['total']))
 
+        # Вытаскиваю инфу из БД для графика -  Кол-во комментариев по дням
+        day_countcomments = Choice.objects.values ('date').annotate (total=Count('id'))
+        day = []
+        count_comm = []
+        for day_count in day_countcomments:
+            day.append (day_count ['date'])
+            count_comm.append ((day_count ['total']))
+
+        # Вытаскиваю инфу из БД для графика -  Кол-во уникальных комментаторов по дням
+        day_countuser = Choice.objects.values ('date').annotate (total=Count('id_user', distinct=True))
+        day1 = []
+        count_user1 = []
+        for day_count in day_countuser:
+            day1.append (day_count ['date'])
+            count_user1.append(day_count['total'])
+
+
+
         fig = plt.figure ()
         ax_1 = fig.add_subplot (2, 2, 1)
         ax_2 = fig.add_subplot (2, 2, 2)
+        ax_3 = fig.add_subplot (2, 2, 3)
+        ax_4 = fig.add_subplot (2, 2, 4)
+
 
         ax_1.set (title='Пользователи с наибольшим количеством комментариев',xlabel='id пользователей',
                   ylabel='Кол-во комментариев')
         ax_2.set (title='Пользователи с наибольшим количеством лайков', xlabel='id пользователей',
                   ylabel='Кол-во лайков')
+        ax_3.set (title='Количество комментариев по дням', xlabel='Дата',
+                  ylabel='Кол-во комментариев')
+        ax_4.set (title='Количество уникальных пользователей учавствующих в обсуждениях по дням', xlabel='Дата',
+                  ylabel='Кол-во пользователей')
         ax_1.bar(user,count)
+        ax_1.set_xticklabels(user, rotation=90)
         ax_2.bar(user1,count1)
+        ax_2.set_xticklabels (user1, rotation=90)
+        ax_3.plot(day, count_comm)
+        ax_3.set_xticklabels (day, rotation=90)
+        ax_4.plot (day1, count_user1)
+        ax_4.set_xticklabels (day1, rotation=90)
         plt.show ()
 
         domainform = DomainForm ()
